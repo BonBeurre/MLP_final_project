b0d8d6c841e1a6f58d1612899939ad119f63e70a
Alexandre_Dibon <alexandre.dibon@efrei.net>
2025-01-07 21:35:39 +0100

    beaucoup de trucs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:8000/predict/", {
+            const response = await fetch("https://soft-serena-adibon-43b0a5e4.koyeb.app/predict/", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
