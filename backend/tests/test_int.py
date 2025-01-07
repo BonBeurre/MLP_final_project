d1bf6e05494c7c273764ae84696401d7421d4b22
Alexandre_Dibon <alexandre.dibon@efrei.net>
2025-01-07 22:38:57 +0100

    deployment

--- backend/tests/test_int.py
+++ backend/tests/test_int.py
@@ -1,11 +1,13 @@
 from fastapi.testclient import TestClient
-from app import app 
 from unittest.mock import patch
+
+from app import app
 
 client = TestClient(app)
 
+
 def test_predict_valid_data():
-    
+
     input_data = {
         "sepal_length": 5.1,
         "sepal_width": 3.5,
@@ -14,7 +16,7 @@ def test_predict_valid_data():
     }
     response = client.post("/predict/", json=input_data)
 
-    assert response.status_code == 200 
+    assert response.status_code == 200
     assert "prediction" in response.json()  # Vérifier 'prediction' est dans la réponse
 
 
@@ -23,11 +25,12 @@ def test_predict_invalid_data():
     invalid_data = {"invalid_feature": "not_a_number"}
     response = client.post("/predict/", json=invalid_data)
 
-    assert response.status_code == 400 
-    assert response.json()["detail"] == "Invalid input format"  # Vérifier le message d'erreur
+    assert response.status_code == 400
+    assert (
+        response.json()["detail"] == "Invalid input format"
+    )  # Vérifier le message d'erreur
 
 
-    
 def test_invalid_http_method():
     response = client.get("/predict/")
     assert response.status_code == 405  # Méthode pas autorisée
