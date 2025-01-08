225223d30cf8d61c0a060acc72fe032ea2dac272
Alexandre_Dibon <alexandre.dibon@efrei.net>
2025-01-08 09:38:43 +0100

    nettoyage

--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,9 @@ from utils import is_correct_format
 app = FastAPI()
 
 origins = [
-    "https://easy-dominica-adibon-6ea94c08.koyeb.app",  # Your frontend origin
+    "https://easy-dominica-adibon-6ea94c08.koyeb.app",
 ]
 
-# Add CORS middleware to allow specific origins
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,22 +24,18 @@ app.add_middleware(
 # Configurer l'URI MLflow pour DagsHub
 mlflow.set_tracking_uri("https://dagshub.com/djayos/my-first-repo.mlflow")
 
-# Authentification pour DagsHub
-os.environ["MLFLOW_TRACKING_USERNAME"] = "djayos"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "21998e1af3bfa82f989a91f83e26d6c3e072488d"
-
 # Charger le modèle enregistré comme "Production" dans le Model Registry
 model = mlflow.pyfunc.load_model("models:/IrisModelRegistry/Production")
 
 
-# Endpoint pour la prédictionnb
+# Endpoint pour la prédiction
 @app.post("/predict/")
 def predict(data: dict):
     """
     Reçoit les données sous forme de JSON et retourne une prédiction.
     """
-    if not is_correct_format(data):  # Valider le format des données
+    if not is_correct_format(data):
         raise HTTPException(status_code=400, detail="Invalid input format")
-    df = pd.DataFrame([data])  # Convertir les données JSON en DataFrame
-    prediction = model.predict(df)  # Effectuer la prédiction
-    return {"prediction": prediction.tolist()}  # Retourner la prédiction
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)
+    return {"prediction": prediction.tolist()}
--- frontend/src/__tests__/e2e_test.js
+++ frontend/src/__tests__/e2e_test.js
@@ -6,7 +6,7 @@ describe("E2E Test - Prédiction Iris", () => {
 
     beforeAll(async () => {
         const options = new chrome.Options();
-        options.addArguments('--headless=new'); // New syntax for headless mode
+        options.addArguments('--headless=new');
         options.addArguments('--no-sandbox');
         options.addArguments('--disable-dev-shm-usage');
 
@@ -17,32 +17,28 @@ describe("E2E Test - Prédiction Iris", () => {
     }, 30000);
 
     afterAll(async () => {
-        if (driver) {  // Check if driver exists before quitting
+        if (driver) {
             await driver.quit();
         }
     });
 
     it("Remplit le formulaire, soumet et affiche la prédiction", async () => {
         try {
-            // Naviguer vers l'application déployée
+
             await driver.get("https://easy-dominica-adibon-6ea94c08.koyeb.app");
 
-            // Remplir les champs du formulaire
             await driver.findElement(By.name("sepal_length")).sendKeys("5.1");
             await driver.findElement(By.name("sepal_width")).sendKeys("3.5");
             await driver.findElement(By.name("petal_length")).sendKeys("1.4");
             await driver.findElement(By.name("petal_width")).sendKeys("0.2");
 
-            // Soumettre le formulaire
             await driver.findElement(By.css("button[type='submit']")).click();
 
-            // Attendre que la prédiction soit affichée
             const predictionElement = await driver.wait(
                 until.elementLocated(By.css("h2")),
                 10000
             );
 
-            // Vérifier la prédiction
             const predictionText = await predictionElement.getText();
             expect(predictionText).toContain("Prédiction : Iris-setosa");
         } catch (error) {
